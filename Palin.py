633055c39d19238238cc64e4768d9345a5d99c6a
cjin1510 <125084265+cjin1510@users.noreply.github.com>
2023-04-13 22:00:23 -0700

    Update Palin.py

--- Palin.py
+++ Palin.py
@@ -1,9 +1,7 @@
 def palindrome(lst):
-    # Check if the list is empty or contains only one element
     if len(lst) <= 1:
         return True
 
-    # Check if the first and last elements are the same
     if lst[0] == lst[-1]:
         return palindrome(lst[1:-1])
     else:
