0da64c6d52b76df5f392494f9a419c7ddecbb641
cjin1510 <125084265+cjin1510@users.noreply.github.com>
2023-04-29 19:30:31 -0700

    Update log.py

--- log.py
+++ log.py
@@ -1,1 +1,16 @@
+import time
+
+def timestamp(func):
+    def wrapper(*args, **kwargs):
+        print(f"[{time.ctime()}]")
+        result = func(*args, **kwargs)
+        print(f"[{time.ctime()}]")
+        return result
+    return wrapper
+
+def main():
+    print("Hi")
+main = timestamp(main)
+
+main()
 
