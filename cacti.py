34e9eedc0c5a9419060e9d030101c8a38b367d61
cjin1510 <125084265+cjin1510@users.noreply.github.com>
2023-04-29 19:33:21 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -1,1 +1,29 @@
+def cacti_number(plot):
+    count = 0
+    rows, cols = len(plot), len(plot[0])
+    for i in range(rows):
+        for j in range(cols):
+            if plot[i][j] == 0:
+                adjacent = False
+                # Check for adjacent cacti horizontally and vertically
+                if i > 0 and plot[i-1][j] == 1:
+                    adjacent = True
+                elif i < rows-1 and plot[i+1][j] == 1:
+                    adjacent = True
+                elif j > 0 and plot[i][j-1] == 1:
+                    adjacent = True
+                elif j < cols-1 and plot[i][j+1] == 1:
+                    adjacent = True
+                # Check for adjacent cacti diagonally
+                elif i > 0 and j > 0 and plot[i-1][j-1] == 1:
+                    adjacent = True
+                elif i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
+                    adjacent = True
+                elif i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
+                    adjacent = True
+                elif i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
+                    adjacent = True
+                if not adjacent:
+                    count += 1
+    return count
 
